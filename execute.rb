f2cf585f6b34de89422e4e72f667dd5db324fe88
Eugene Yevtushenko <freeman.zhenia@gmail.com>
2014-04-09 01:28:06 +0300

    Adds attributes validating

--- execute.rb
+++ execute.rb
@@ -1,2 +1,26 @@
 Dir.glob("#{File.dirname(__FILE__)}/config/*").each { |file| require_relative file }
 Dir.glob("#{File.dirname(__FILE__)}/lib/*").each { |file| require_relative file }
+
+smtp_config = {
+    host: 'mailtrap.io',
+    port: 2525,
+    address: 'mailtrap.io',
+    user_name: '250929978b732beb',
+    password: '63d5ecc2b2b98e',
+    authentication: :cram_md5
+}
+
+monitor_config = {
+    site_url: "http://ya.ru",
+    email: "freeman.zhenia@gmail.com",
+    appropriate_response_codes: [200, 201, 301],
+    number_of_tries: 3,
+    max_response_time: 30
+}
+
+yandex_monitor = SiteMonitor.new(monitor_config, smtp_config)
+validate_errors = yandex_monitor.validate_attributes
+if validate_errors.size > 0
+  puts validate_errors
+  exit
+end
--- lib/site_monitor.rb
+++ lib/site_monitor.rb
@@ -3,7 +3,7 @@ class SiteMonitor
   attr_accessor :site_url, :email, :appropriate_response_codes,
                 :number_of_tries, :max_response_time
 
-  attr_reader :smtp_config_array
+  attr_reader :smtp_config_array, :valid_attributes
 
   def initialize(options={}, mail_config)
     @site_url = options[:site_url]
@@ -12,6 +12,20 @@ class SiteMonitor
     @number_of_tries = options[:number_of_tries]
     @max_response_time = options[:max_response_time]
     @smtp_config_array = mail_config.values
+    @valid_attributes = false
+  end
+
+  def validate_attributes
+    errors = []
+    errors << "Incorrect url address" if @site_url.match(/https?:\/\/[\S]+/).nil?
+    errors << "Incorrect email address" if @email.match(/\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/).nil?
+    errors << "You should pass correct response codes in array" if @appropriate_response_codes.class != Array
+    errors << "Incorrect number of tries" if @number_of_tries.class != Fixnum
+    errors << "Incorrect max response time" if @max_response_time.class != Fixnum
+    if errors == []
+      @valid_attributes = true
+    end
+    errors
   end
 end
 
